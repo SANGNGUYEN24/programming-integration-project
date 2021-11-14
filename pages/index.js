b753cef6115373ff1d0cb27a9559f65b10078740
Sang Nguyen <sangnguyendinh24work@gmail.com>
2021-11-14 10:34:26 +0700

    Firebase config

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import Header from "../components/Header";
 import Nav from "../components/Nav";
 import Results from "../components/Results";
 import requests from "../utils/requests";
+import firebaseApp from "../services/config";
+// Initialize firebase
+firebaseApp();
 
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
---
+++ services/config.js
@@ -0,0 +1,24 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = () => {
+    initializeApp(firebaseConfig);
+    console.log("Initialized Firebase!");
+}
+
+// export const firebaseAuth = getAuth(app);
+// export const firestore = getFirestore(app);
+
+export default firebaseApp;
